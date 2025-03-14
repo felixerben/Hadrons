385da8a38f97b926beb66429cb6b65e727a188fb
felixerben <felix.erben@ed.ac.uk>
2025-03-14 12:16:23 +0000

    new branch

---
+++ Hadrons/Modules/MSolver/MultiRHSMultiGrid.cpp
@@ -0,0 +1,7 @@
+#include <Hadrons/Modules/MSolver/MultiRHSMultiGrid.hpp>
+
+using namespace Grid;
+using namespace Hadrons;
+using namespace MSolver;
+
+template class HADRONS_NAMESPACE::MSolver::TMultiRHSMultiGrid<FIMPL>;
